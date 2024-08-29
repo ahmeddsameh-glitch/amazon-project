3898d66c91e34e0dc00185d0af7d198281b2594f
ahmed <a.samehpsn@gmail.com>
2024-08-29 12:23:40 +0200

    cart changed from const to let , generated removefrom cart and imported it in checkout.js

--- amazon-project-base/data/cart.js
+++ amazon-project-base/data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [{
+export let cart = [{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
 },{
@@ -22,4 +22,12 @@ if (matchingItem) {
         quantity: 1
     });
 }
+}
+export function removeFromCart(productId){
+    const newCart = [];
+    cart.forEach((cardItem) => {
+        if(cardItem.productId!== productId)
+        newCart.push(cardItem);
+    });
+    cart = newCart;
 }
