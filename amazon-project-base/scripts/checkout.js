d48102b55f9395807b93eb7a62394d30618c3100
ahmed <a.samehpsn@gmail.com>
2024-08-29 12:28:52 +0200

    delete button is now interactive

--- amazon-project-base/scripts/checkout.js
+++ amazon-project-base/scripts/checkout.js
@@ -13,7 +13,8 @@ cart.forEach((cartItem)=>{
     console.log(matchingProduct);
     
 cartSummaryHTML +=`
-  <div class="cart-item-container">
+  <div class="cart-item-container 
+  js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">
         Delivery date: Tuesday, June 21
     </div>
@@ -96,6 +97,6 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
 link.addEventListener('click',()=>{
 const productId = link.dataset.productId ;
 removeFromCart(productId);
-console.log(cart);
+const container = document.querySelector(`.js-cart-item-container-${productId}`).remove();
 });
 });
