a423f06412cfc481d0aefe5db6f99abd87946883
ahmed <a.samehpsn@gmail.com>
2024-09-07 14:07:21 +0200

    create automated tests for add to cart

--- amazon-project-base/data/cart.js
+++ amazon-project-base/data/cart.js
@@ -1,53 +1,54 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
-if(!cart){
-cart = [{
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-    deliveryOptionsId : '1'
-},{
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity:1,
-    deliveryOptionsId : '2'
-}
-];
-}
-// Normalizing the data 
-function saveToStorage(){
-    localStorage.setItem('cart', JSON.stringify(cart));
-}
-export function addToCart(productId){
-    let matchingItem;
-cart.forEach((cardItem) => {
-    if(cardItem.productId === productId)
-    matchingItem = cardItem; 
-});
-if (matchingItem) {
-
-    matchingItem.quantity += 1;
-} else {
-    cart.push({
-        productId: productId,
+export let cart;
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem("cart"));
+  if (!cart) {
+    cart = [
+      {
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2,
+        deliveryOptionsId: "1",
+      },
+      {
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
         quantity: 1,
-        deliveryOptionsId : '1' // Default delivery option for new items
-    });
+        deliveryOptionsId: "2",
+      },
+    ];
+  }
 }
-saveToStorage();
+// Normalizing the data
+function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
 }
-export function removeFromCart(productId){
-    const newCart = [];
-    cart.forEach((cardItem) => {
-        if(cardItem.productId!== productId)
-        newCart.push(cardItem);
+export function addToCart(productId) {
+  let matchingItem;
+  cart.forEach((cardItem) => {
+    if (cardItem.productId === productId) matchingItem = cardItem;
+  });
+  if (matchingItem) {
+    matchingItem.quantity += 1;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+      deliveryOptionsId: "1", // Default delivery option for new items
     });
-    cart = newCart;
-    saveToStorage();    
+  }
+  saveToStorage();
 }
-export function updateDeliveryOption(productId,deliveryOptionsId){
-    let matchingItem;
-    cart.forEach((cardItem) => {
-        if(cardItem.productId === productId)
-        matchingItem = cardItem; 
-    });
-    matchingItem.deliveryOptionsId = deliveryOptionsId;
-    saveToStorage();
+export function removeFromCart(productId) {
+  const newCart = [];
+  cart.forEach((cardItem) => {
+    if (cardItem.productId !== productId) newCart.push(cardItem);
+  });
+  cart = newCart;
+  saveToStorage();
+}
+export function updateDeliveryOption(productId, deliveryOptionsId) {
+  let matchingItem;
+  cart.forEach((cardItem) => {
+    if (cardItem.productId === productId) matchingItem = cardItem;
+  });
+  matchingItem.deliveryOptionsId = deliveryOptionsId;
+  saveToStorage();
 }
--- amazon-project-base/tests-jasmine/utils/moneyTest.js
+++ amazon-project-base/tests-jasmine/utils/moneyTest.js
@@ -1,4 +1,4 @@
-import { formatCurrency } from "../scripts/utils/money.js";
+import { formatCurrency } from "../../scripts/utils/money.js";
 describe("test suite : formatCurrency", () => {
   it("convert cents into dollars ", () => {
     expect(formatCurrency(2095)).toEqual("20.95");
