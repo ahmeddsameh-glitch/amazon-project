53584346978e4c5917dd4ad9797e6f2c179556b2
ahmed <a.samehpsn@gmail.com>
2024-09-07 13:19:43 +0200

    changes on testing

--- amazon-project-base/tests/moneyTest.js
+++ amazon-project-base/tests/moneyTest.js
@@ -1,11 +1,17 @@
-// import { formatCurrency } from "../scripts/utils/money.js";
-// if (formatCurrency(2095) === "20.95") {
-//   console.log("passed");
-// } else {
-//   console.log("failed");
-// }
+import { formatCurrency } from "../scripts/utils/money.js";
+if (formatCurrency(2095) === "20.95") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
 
-// if(formatCurrency(0) === "0.00") {
-
-// }
-log(typeof 42)
+if (formatCurrency(0) === "0.00") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
+if (formatCurrency(2000.5) === "20.01") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
