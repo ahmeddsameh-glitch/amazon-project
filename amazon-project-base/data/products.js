b3f522b42f42e0df17218e9d41b390d792565ddb
ahmed <a.samehpsn@gmail.com>
2024-09-07 21:16:16 +0200

    use inhertiance and polymoriphsim  to add size chart

--- amazon-project-base/data/products.js
+++ amazon-project-base/data/products.js
@@ -27,6 +27,20 @@ class Product {
   getPrice() {
     return `$${formatCurrency(this.priceCents)}`;
   }
+  extraInfoHTML() {
+    return "";
+  }
+}
+class Clothing extends Product {
+  sizeChartLink;
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+  extraInfoHTML() {
+    return `
+    <a href="${this.sizeChartLink}" target="_blank">Size chart</a>`;
+  }
 }
 export const products = [
   {
@@ -500,6 +514,8 @@ export const products = [
     keywords: ["sweaters", "hoodies", "apparel", "mens"],
   },
 ].map((productDetails) => {
+  if (productDetails.type === "clothing") {
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
-console.log(products);
--- amazon-project-base/scripts/amazon.js
+++ amazon-project-base/scripts/amazon.js
@@ -40,7 +40,8 @@ products.forEach((product)=>{
               <option value="10">10</option>
             </select>
           </div>
-
+          
+          ${product.extraInfoHTML()}
           <div class="product-spacer"></div>
 
           <div class="added-to-cart">
